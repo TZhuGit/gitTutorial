328d104f511447b17504f93c516b9b0a123538a6
Tony Zhu <tonyzhu95@hotmail.com>
2017-02-17 22:00:06 -0500

    added course class and made changes to student and instructor

---
+++ src/Course.java
@@ -0,0 +1,11 @@
+/**
+ * Created by T on 2017-02-17.
+ */
+public class Course {
+    String cname;
+    String description;
+    public Course(String cname, String description){
+        this.cname = cname;
+        this.description = description;
+    }
+}
